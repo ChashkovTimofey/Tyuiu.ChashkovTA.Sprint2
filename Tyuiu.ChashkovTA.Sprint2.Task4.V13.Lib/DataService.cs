f70e549366b870901d2593a93248da9b403728b7
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-11-29 21:42:39 +0500

    Task4

---
+++ Tyuiu.ChashkovTA.Sprint2.Task4.V13.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ChashkovTA.Sprint2.Task4.V13.Lib
+{
+    public class DataService : ISprint2Task4V13
+    {
+        public double Calculate(double x, double y)
+        {
+
+
+            double z;
+
+            if (Math.Sqrt(x * 3) > y * 2 - 20)
+            {
+                z = x + 10 * y;
+            }
+            else
+            {
+                z = 1 / x;
+            }
+
+
+            double expressionResult;
+
+            if (y * y - Math.Pow(Math.Sin(y), 2) + 9 == 0)
+           
+            expressionResult = (Math.Pow(x, 2) + Math.Pow(Math.Cos(y), 2) + 13) / (Math.Pow(y, 2) - Math.Pow(Math.Sin(y), 2) + 9);
+
+            
+            return z; }
+        }
+    }
