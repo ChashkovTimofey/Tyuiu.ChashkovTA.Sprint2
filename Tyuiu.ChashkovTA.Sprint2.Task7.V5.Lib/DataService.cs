b326b3d51bbd41ee05b35791b364db94600170e1
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-11-29 23:18:24 +0500

    Task7

---
+++ Tyuiu.ChashkovTA.Sprint2.Task7.V5.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ChashkovTA.Sprint2.Task7.V5.Lib
+{
+    public class DataService : ISprint2Task7V5
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+
+            bool isInRegion = IsPointInRegion(x, y);
+
+            return isInRegion;
+        }
+
+        static bool IsPointInRegion(double x, double y)
+        {
+            if (x < 0 || x > 1 || y < 0 || y > 1) return false; // быстрая проверка на границы области
+
+
+            return (y >= Math.Pow(x, 2)) && (y <= Math.Exp(x)) && (y <= Math.Exp(-x));
+
+        }
+    }
+}
---
+++ Tyuiu.ChashkovTA.Sprint2.Task7.V5/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ChashkovTA.Sprint2.Task7.V5.Lib;
+namespace Tyuiu.ChashkovTA.Sprint2.Task7.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #5                                                            *");
+            Console.WriteLine("* Выполнил: Чашков Тимофей Александрович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.CheckDotInShadedArea(3, 5);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
