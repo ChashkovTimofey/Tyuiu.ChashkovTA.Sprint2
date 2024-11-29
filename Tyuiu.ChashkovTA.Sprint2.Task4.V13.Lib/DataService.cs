68c3d3c1d09172ab3268891c2b0b41cc7c854dff
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-11-29 22:04:55 +0500

    Task4

--- Tyuiu.ChashkovTA.Sprint2.Task4.V13.Lib/DataService.cs
+++ Tyuiu.ChashkovTA.Sprint2.Task4.V13.Lib/DataService.cs
@@ -23,10 +23,12 @@ namespace Tyuiu.ChashkovTA.Sprint2.Task4.V13.Lib
             double expressionResult;
 
             if (y * y - Math.Pow(Math.Sin(y), 2) + 9 == 0)
-           
-            expressionResult = (Math.Pow(x, 2) + Math.Pow(Math.Cos(y), 2) + 13) / (Math.Pow(y, 2) - Math.Pow(Math.Sin(y), 2) + 9);
 
-            
+            {
+                expressionResult = (Math.Pow(x, 2) + Math.Pow(Math.Cos(y), 2) + 13) / (Math.Pow(y, 2) - Math.Pow(Math.Sin(y), 2) + 9);
+
+                Console.WriteLine($"z = {Math.Round(z, 3)}, выражение: {Math.Round(expressionResult, 3)}");
+            }
             return z; }
         }
     }
